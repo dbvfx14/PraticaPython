519666e19f30320e159d7188333f1405d9492763
VIC0143562 <danielbsilva130@gmail.com>
2023-08-01 10:53:36 -0300

    aprendendo python

---
+++ ex_fixacao01.py
@@ -0,0 +1,59 @@
+# Solicita o valor em Dólares para o usuário
+valor_em_dolares = float(input("Digite o valor em Dólares: "))
+
+# Realiza a conversão para Reais
+valor_em_reais = valor_em_dolares * 4.77
+
+# Exibe o resultado da conversão
+print(f"{valor_em_dolares} Dólares equivalem a {valor_em_reais:.2f} Reais.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
